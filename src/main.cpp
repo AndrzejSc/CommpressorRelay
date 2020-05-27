1f92472aa1a199914bd94143f0ca76660f3482a4
AndrzejSc <aschabowski@gmail.com>
2020-05-27 10:52:41 +0200

    Reaguje na komendy RUN/STOP wysyłane do generatora. Przcisk manualnego startu/stopu

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,27 @@
  *          OBIEKTY
  *************************************************************************/
 #define RELAY_PIN 5
-#define LED_PIN 6
+#define RELAY_LED_PIN 6
+#define MANUAL_BUTTON_PIN 8
 
-SoftwareSerial SerialSpy (2,3); // Input (data from devices)
-char buffor;
+
+SoftwareSerial SerialSpy(2, 3); // Input (data from devices)
+unsigned char buffor;
 String commandString;
-String commandStop1 = "STO\r";
-String commandStop2 = "STOP";
-String commandRun1 = "RUN\r";
+//String commandStop1 = "STO\r";
+//String commandStop2 = "STOP";
+//String commandRun1 = "RUN\r";
 
 bool relayState;
+int manualButtonState = LOW;
+
+
+// Debouncing
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 200;    // the debounce time; increase if the output flickers
 
 //Funkcje
-void  parseAndDoCommand (String command);
+void parseAndDoCommand(const String& command);
 
 
 /**************************************************************************
@@ -30,18 +38,26 @@ void setup() {
     Serial.begin(2400);
     SerialSpy.begin(2400);
     Serial.println("Spy is spying...");
+
+    // Set input/output
     pinMode(RELAY_PIN, OUTPUT);
-    pinMode(LED_PIN, OUTPUT);
-    delay(1000);
-    digitalWrite(RELAY_PIN, HIGH);
-    Serial.println("Relay is ON");
-    digitalWrite(LED_PIN, HIGH);
+    pinMode(RELAY_LED_PIN, OUTPUT);
+
+    // Drugi pin guzika do masy, normalny stan przycisku = HIGH
+    pinMode(MANUAL_BUTTON_PIN, INPUT_PULLUP);       // Pin podciągniety do VCC przez rezystor.
+
+    //delay(1000);
+
+    // Ustalam poczatkowe wartosci pinow
+    digitalWrite(RELAY_PIN, LOW);
+    relayState = false;
+    manualButtonState = LOW;
 }
 
 void loop() {
     while (SerialSpy.available()) {
         buffor = SerialSpy.read();
-        if (buffor=='\n') {
+        if (buffor == '\n') {
             //Serial.print("Spy said: ");
             // Serial.println(commandString);
             parseAndDoCommand(commandString);
@@ -53,22 +69,38 @@ void loop() {
         }
     }
 
-    // Sprawdzam stan przekaznika, jesli on - zaswiecam diode
-    relayState ? digitalWrite(LED_PIN, HIGH) : digitalWrite(LED_PIN, LOW);
+    // TODO check it!
+    // Sprawdzam czy stan przekaźnika zostal zmieniony przez COM, jesli tak, zmieniam diodę i status na buttonie
+    if (relayState) {
+        digitalWrite(RELAY_LED_PIN, HIGH);
+        manualButtonState = true;
+    } else {
+        digitalWrite(RELAY_LED_PIN, LOW);
+        manualButtonState = false;
+    }
 
+    int readButtonState = digitalRead(MANUAL_BUTTON_PIN);
+    // sprawdzam, czy nacisnieto guzik i przytrzymano go > delayDeboucne
+    if (readButtonState == LOW && millis() - lastDebounceTime > debounceDelay) {
+        relayState = !manualButtonState;
+        lastDebounceTime = millis();
+    }
 }
-
-void parseAndDoCommand (String command) {
-    Serial.print("COMMAND: "); Serial.println(command);
-  if (command == "STO") {
-        Serial.println ("Spy turns compressor OFF");
+// Odbieramy 'command' jako referencje - czyli odbieramy sam adres do obiektu command (nie jest przesyłana kopia obiektu)
+// ale traktujemy go jako zwykly obiekt ( a nie jako adres). Alias const, mówi iż, obiekt nie będzie zmieniany w tej funkcji.
+void parseAndDoCommand(const String& command) {
+    Serial.print("RECEIVED COMMAND: ");
+    Serial.println(command);
+    if (command == "STO") {
+        // Relay to 0
+        Serial.println("\tSpy turns compressor OFF");
         digitalWrite(RELAY_PIN, LOW);
         relayState = false;
     } else if (command == "RUN") {
-    // Relay to 1
-        Serial.println ("Spy turns compressor ON");
+        // Relay to 1
+        Serial.println("\tSpy turns compressor ON");
         digitalWrite(RELAY_PIN, HIGH);
-        relayState = false;
+        relayState = true;
     } else {
         //Serial.println ("Spy doesn't know what to do");
     }
